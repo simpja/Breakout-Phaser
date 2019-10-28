7b5bababca93e5fbd4d0de1f732c04acd5667c72
Simen Stenersen Pjaaten <simenpjaat@gmail.com>
2019-10-28 19:26:17 +0100

    Need help debouncing ballLeaveScreen function

--- game/Js/game.js
+++ game/Js/game.js
@@ -14,10 +14,6 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      // x: 0,
-      // y: 0,
-      // width: scene.sys.scale.width,
-      // height: scene.sys.scale.height,
       gravity: {
         x: 0,
         y: 0
@@ -31,6 +27,7 @@ var game = new Phaser.Game(config);
 
 // Variable decalaration
 var bounds;
+var ballIsOut;
 var ball;
 var paddleBottom;
 var paddleTop;
@@ -54,12 +51,14 @@ function preload() {
   this.load.image("paddle", "Assets/img/paddle.png");
   this.load.image("brick", "Assets/img/brick.png");
   this.load.image("background", "Assets/img/starsBackground.jpg")
-  console.log(game.config.width);
 }
 
 function create() {
+  //Add image as background
   this.add.image(0, 0, 'background').setOrigin(0, 0);
-  //this.physics.startSystem(Phaser.Physics.ARCADE);
+  
+  // Add our images (ball, paddles) as sprites, objects that we can assign physical properties and animation. 
+  // sprites live in the scope of the scene
   ball = this.physics.add.sprite(
     game.config.width * 0.5, //was this.world.widht * 0.5;
     game.config.height - 45,
@@ -71,42 +70,38 @@ function create() {
     "paddle"
   );
   paddleTop = this.physics.add.sprite(game.config.width / 2, 0, "paddle");
-  // Adds our images (ball, paddle) into a sprite, an object that we can assign physical properties later
+
+  // Define the Origo of the sprite. 
+  // when we position the x value of the sprite we now position the middle of the sprite, instead of the default left edge as in phaser 2.
   ball.setOrigin(0, 0.5);
   paddleBottom.setOrigin(0.5, 1);
   paddleTop.setOrigin(0.5, 1);
-  // Defines the Origo of the sprite. when we position the x value of the
-  // sprite we now position the middle of the sprite, instead of the default left edge.
+
+  // Adjust the scales of the sprites so that they fit relative to each other. 
+  // We scale them down in x and y direction respectively (the two input parameters)
   ball.setScale(0.2, 0.2);
   paddleBottom.setScale(0.7, 0.2);
   paddleTop.setScale(-0.7, -0.2);
-  // The image is too big, so we scale it down equally x and y (the two input parameters)
 
+  // Make ball collide with the bounds of the world.
   ball.body.setCollideWorldBounds(true);
+  // But we don't want it to collide with the bottom!
   this.physics.world.checkCollision.down = false;
-  ball.setBounce(1);
-  ball.body.velocity.set(200, -200);
-
- 
-
-  // logic for executing function when ball leaves screen
-  // Old solution that doesn't work anymore...
-   // ball.checkWorldBounds = true;
-  //ball.events.onOutOfBounds.add(ballLeaveScreen, this);
-
-  // new solution!
-  //var bounds = this.physics.world.bounds.width;
+  // save bounds for checking when ball leaves screen in update function later in the script
   bounds = this.physics.world.bounds;
+  // Let's make the ball bounce with zero loss - all energy is saved througout the collision.
+  ball.setBounce(1);
+  // And give the ball an initial speed and direction.
+  ball.body.velocity.set(200, -200);
   
-//  var spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -20, -20);
-
-  paddleBottom.body.immovable = true; //Makes the paddle unmovable when colliding with the ball
+  //Make the paddle unmovable when colliding with the ball
+  paddleBottom.body.immovable = true; 
+  paddleTop.body.immovable = true; 
 
   // Test av friction
   // paddleBottom.body.friction.x = 1; //Looks good, doesn't work.
-
-  paddleTop.body.immovable = true; //Makes the paddle unmovable when colliding with the ball
   
+  // Make the text objects to write lives remaining and score in.
   scoreText = this.add.text(5, 5, "Points: " + score, {
     font: "10px Arial",
     fill: "#0095DD"
@@ -128,12 +123,14 @@ function create() {
   livesLostText.setOrigin(1, 0);
   livesLostText.visible = false;
 
-  // Adding arrow key movement
+  // Adding arrow key + shift + spacebar listener as one common cursors-object
   cursors = this.input.keyboard.createCursorKeys();
-  //spacebar = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-  //enter = this.input.keyboard.addKey(Phaser.KeyCode.ENTER);
+
+  // we have a boolean that tells us if the ball is out of bounds
+  ballIsOut = false;
 };
 
+// Outside the functions we make these socket-connections that adjust the paddle positions when a user of the controllers emit an event.
 socket.on("set-position-bottom", x => {
   setXpos(paddleBottom, x);
 });
@@ -155,24 +152,13 @@ function update() {
     paddleBottom.x += 5;
   }
 
-  // Check if ball is out of bounds 
-  
-  /*
-      // Get all physics bodies within the physics rectangle (same as the camera rectangle for this scene.)
-      var within = this.physics.overlapRect(bounds.x, bounds.y, bounds.width, bounds.height);
-  
-      within.forEach(function (body) {
-          body.gameObject.setTint(0xff0000);
-          console.log(body)
-      });
-      
-*/
-      if (Phaser.Geom.Rectangle.Overlaps(bounds, ball.getBounds()) != true){
-        ballLeaveScreen();
-      }
-    
-     //console.log(Phaser.Geom.Rectangle.Overlaps(bounds, ball.getBounds()))
 
+  // Check if ball is out of bounds 
+  // Need to only run the ballLeaveScreen() function once!!!!
+  if (Phaser.Geom.Rectangle.Overlaps(bounds, ball.getBounds()) != true){
+    // this.physics.pause(); // might use this when resetting the ball
+    ballLeaveScreen();
+  };
 };
 
 // Functions following!
@@ -187,13 +173,20 @@ function setXpos(paddle, newX) {
 }
 
 function ballLeaveScreen() {
+  //if (ballIsOut) return;
+  //this.physics.pause();
   lives--;
-  console.log('hi');
+  ball.disableBody(true, true);
+  console.log('hi ballLeave');
   if (lives) {
     livesText.setText("Lives: " + lives);
     livesLostText.visible = true;
-    ball.reset(game.config.width / 2, game.config.height - 45);
-    paddleBottom.reset(game.config.width / 2, game.config.height);
+    ball.x = game.scene.width / 2;
+    ball.y = game.scene.height / 2;
+    // ball.reset(game.config.width / 2, game.config.height - 45);
+    ballIsOut = false;
+    
+    //paddleBottom.reset(game.config.width / 2, game.config.height);
 
     if (cursors.space.isDown) {
       livesLostText.visible = false;
@@ -204,3 +197,26 @@ function ballLeaveScreen() {
     location.reload();
   }
 }
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  console.log('hi debounce');
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Tried to store the debounced function as a variable, but it only runs the function...
+// var debouncedBallLeaveScreen = debounce(ballLeaveScreen, 3000);
