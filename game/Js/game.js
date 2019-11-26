211d176943062b489a15359ee06ed22c41c81279
Rikard Eide <rikardeide@gmail.com>
2019-11-26 22:03:28 +0100

    autofix some eslint errors

--- game/Js/game.js
+++ game/Js/game.js
@@ -1,72 +1,72 @@
 // Initiate socket object
-var socket = io();
+const socket = io();
 
 // Define a config object for Phaser3
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 480,
   height: 320,
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    preload,
+    create,
+    update,
   },
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: {
         x: 0,
-        y: 0
-      }
-    }
-  }
+        y: 0,
+      },
+    },
+  },
 };
 
 // Initiate phaser game object using HTML Canvas
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 // Variable decalaration
-var bounds;
-var frictionFactor = 20; // hehe, fakk physics.
-var paddleMoveStep = 5;
-var gameStatus = "start";
-var bottomFence;
-var ball;
-var paddleBottom;
-var paddleTop;
-var bricks;
-var newBrick;
-var brickInfo;
-var score = 0;
-var lives = 3;
+
+const frictionFactor = 20; // hehe, fakk physics.
+const paddleMoveStep = 5;
+let gameStatus = 'start';
+let bottomFence;
+let ball;
+let paddleBottom;
+let paddleTop;
+let bricks;
+let newBrick;
+let brickInfo;
+const score = 0;
+let lives = 3;
 let livesText;
 let livesLostText;
-var cursors;
-var spacebar;
-var paddlePrevBottom = {
+let cursors;
+let spacebar;
+const paddlePrevBottom = {
   x: 1,
-  y: 1
+  y: 1,
 };
-var paddlePrevTop = {
+const paddlePrevTop = {
   x: 1,
-  y: 1
+  y: 1,
 };
 
 function preload() {
-  //this.scale.pageAlignHorizontally = true;
-  //Aligns the canvas in the middle between top and bottom of the screen
-  //this.scale.pageAlignVertically = true;
-  //The same as over, but in the middle of left and right
-  this.load.image("ball", "Assets/img/ball.png");
-  this.load.image("paddle", "Assets/img/paddle.png");
-  this.load.image("brick", "Assets/img/brick.png");
-  this.load.image("background", "Assets/img/starsBackground.jpg");
-  this.load.image("fence", "Assets/img/fence.png");
+  // this.scale.pageAlignHorizontally = true;
+  // Aligns the canvas in the middle between top and bottom of the screen
+  // this.scale.pageAlignVertically = true;
+  // The same as over, but in the middle of left and right
+  this.load.image('ball', 'Assets/img/ball.png');
+  this.load.image('paddle', 'Assets/img/paddle.png');
+  this.load.image('brick', 'Assets/img/brick.png');
+  this.load.image('background', 'Assets/img/starsBackground.jpg');
+  this.load.image('fence', 'Assets/img/fence.png');
 }
 
 function create() {
-  //Add image as background
-  this.add.image(0, 0, "background").setOrigin(0, 0);
+  // Add image as background
+  this.add.image(0, 0, 'background').setOrigin(0, 0);
 
   createHud(this);
 
@@ -75,7 +75,7 @@ function create() {
   bottomFence = this.physics.add.sprite(
     game.config.height - 5,
     game.config.width,
-    "fence"
+    'fence',
   );
   // We don't want our fence to move (:
   bottomFence.body.immovable = true;
@@ -83,16 +83,16 @@ function create() {
   // Add our images (ball, paddles) as sprites, objects that we can assign physical properties and animation.
   // sprites live in the scope of the scene
   ball = this.physics.add.sprite(
-    game.config.width * 0.5, //was this.world.width * 0.5;
+    game.config.width * 0.5, // was this.world.width * 0.5;
     game.config.height - 75,
-    "ball"
+    'ball',
   );
   paddleBottom = this.physics.add.sprite(
     game.config.width / 2,
     game.config.height,
-    "paddle"
+    'paddle',
   );
-  paddleTop = this.physics.add.sprite(game.config.width / 2, 0, "paddle");
+  paddleTop = this.physics.add.sprite(game.config.width / 2, 0, 'paddle');
 
   // Define the Origo of the sprite.
   // when we position the x value of the sprite we now position the middle of the sprite, instead of the default left edge as in phaser 2.
@@ -110,47 +110,46 @@ function create() {
   ball.body.setCollideWorldBounds(true);
   // But we don't want it to collide with the bottom!
   this.physics.world.checkCollision.down = false;
-  // save bounds for checking when ball leaves screen in update function later in the script
-  bounds = this.physics.world.bounds;
+
   // Let's make the ball bounce with zero loss - all energy is saved througout the collision.
   ball.setBounce(1);
   // And give the ball an initial speed and direction.
   ball.body.velocity.set(200, -200);
 
-  //Make the paddle unmovable when colliding with the ball
+  // Make the paddle unmovable when colliding with the ball
   paddleBottom.body.immovable = true;
   paddleTop.body.immovable = true;
 
   // Adding arrow key + shift + spacebar listener as one common cursors-object
   cursors = this.input.keyboard.createCursorKeys();
 
-  gameStatus = "running";
+  gameStatus = 'running';
   this.physics.add.collider(ball, paddleTop, strikeBall, null, this);
   this.physics.add.collider(ball, paddleBottom, strikeBall, null, this);
   this.physics.add.overlap(ball, bottomFence, ballLeaveScreen, null, this);
 }
 
 // Outside the functions we make these socket-connections that adjust the paddle positions when a user of the controllers emit an event.
-socket.on("set-position-bottom", x => {
+socket.on('set-position-bottom', x => {
   setXpos(paddleBottom, x);
 });
-socket.on("set-position-top", x => {
+socket.on('set-position-top', x => {
   setXpos(paddleTop, x);
 });
 
 function update() {
   // Check if game is paused, and reset if space is pushed
-  if (cursors.space.isDown && gameStatus == "paused") {
+  if (cursors.space.isDown && gameStatus == 'paused') {
     livesLostText.visible = false;
     ball.enableBody(
       true,
       game.config.width / 2,
       game.config.height / 2,
       true,
-      true
+      true,
     );
     ball.body.velocity.set(150, -150);
-    gameStatus = "running";
+    gameStatus = 'running';
   }
 
   // Save current positions of paddles in order to simulate friction
@@ -172,23 +171,23 @@ function update() {
 
 function createHud(scene) {
   // Make the text objects to write lives remaining and score in.
-  const scoreText = scene.add.text(5, 5, "Points: " + score, {
-    font: "10px Arial",
-    fill: "#0095DD"
+  const scoreText = scene.add.text(5, 5, `Points: ${score}`, {
+    font: '10px Arial',
+    fill: '#0095DD',
   });
-  livesText = scene.add.text(game.config.width - 5, 5, "Lives: " + lives, {
-    font: "10px Arial",
-    fill: "#0095DD"
+  livesText = scene.add.text(game.config.width - 5, 5, `Lives: ${lives}`, {
+    font: '10px Arial',
+    fill: '#0095DD',
   });
   livesText.setOrigin(1, 0);
   livesLostText = scene.add.text(
     game.config.width / 2,
     game.config.height / 2,
-    "Life lost, press space to continue",
+    'Life lost, press space to continue',
     {
-      font: "10px Arial",
-      fill: "#0095DD"
-    }
+      font: '10px Arial',
+      fill: '#0095DD',
+    },
   );
   livesLostText.setOrigin(1, 0);
   livesLostText.visible = false;
@@ -211,20 +210,20 @@ function strikeBall(ball, paddle) {
   // Currently only run this function on paddleBottom..
   if (ball.y > 200) {
     ball.body.setVelocityX(
-      ball.body.velocity.x + frictionFactor * (paddle.x - paddlePrevBottom.x)
+      ball.body.velocity.x + frictionFactor * (paddle.x - paddlePrevBottom.x),
     );
   }
 }
 
 function ballLeaveScreen() {
-  gameStatus = "paused";
+  gameStatus = 'paused';
   ball.disableBody(true, false);
   lives--;
   if (lives) {
-    livesText.setText("Lives: " + lives);
+    livesText.setText(`Lives: ${lives}`);
     livesLostText.visible = true;
   } else {
-    alert("Game over!");
+    alert('Game over!');
     location.reload();
   }
 }
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const express = require("express");
+const http = require('http');
+const express = require('express');
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
 const port = 3000;
 
 // Serve the game at localhost:3000/game/ using express static files.
-app.use("/game/", express.static(__dirname + "/game/"));
+app.use('/game/', express.static(`${__dirname}/game/`));
 
 // Serve the controllers at localhost:[port]/controllerTop/ and localhost:[port]/controllerBottom/ using static files. Spesifying the html-file since it is not called index.html.
-app.use("/controller/", express.static(__dirname + "/controller/"));
-app.get("/controllerBottom/*", function(req, res) {
-  res.sendFile(__dirname + "/controller/" + "controllerBottom.html");
+app.use('/controller/', express.static(`${__dirname}/controller/`));
+app.get('/controllerBottom/*', (req, res) => {
+  res.sendFile(`${__dirname}/controller/controllerBottom.html`);
 });
-app.get("/controllerTop/*", function(req, res) {
-  res.sendFile(__dirname + "/controller/" + "controllerTop.html");
+app.get('/controllerTop/*', (req, res) => {
+  res.sendFile(`${__dirname}/controller/controllerTop.html`);
 });
 
 // Handle socket stuff!
-io.on("connection", function(socket) {
-  console.log("someone connected!");
+io.on('connection', socket => {
+  console.log('someone connected!');
 
-  socket.on("set-position-bottom", event => {
-    io.emit("set-position-bottom", event);
+  socket.on('set-position-bottom', event => {
+    io.emit('set-position-bottom', event);
     // console.log("url" + socket.handshake.url);
   });
 
-  socket.on("set-position-top", event => {
-    io.emit("set-position-top", event);
+  socket.on('set-position-top', event => {
+    io.emit('set-position-top', event);
     // console.log("hei controller 2");
   });
 });
 
-
-http.listen(port, function() {
+server.listen(port, () => {
   console.log(`listening on: ${port}`);
 });
