e741ff4504ad8925b4bce8c727e4f9cf54795e4a
Simen Pjaaten <simenpjaat@gmail.com>
2020-07-28 21:40:54 +0200

    Put routes in separate files to clean up server script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
-// Do work here
+// For our home screen we add a route to the empty directory
+// the route simply serves the static html file in the home folder.
 router.get("/", (req, res) => {
-  res.render("../instructions/index");
+  res.sendFile(path.resolve("./home/index.html"));
+});
+
+router.get("/instructions", (req, res) => {
+  res.sendFile(path.resolve("./instructions/index.html"));
+});
+
+router.get("/game", (req, res) => {
+  res.sendFile(path.resolve("./game/index.html"));
+});
+
+router.get("/game", (req, res) => {
+  res.sendFile(path.resolve("./game/index.html"));
+});
+
+router.get("/controller", (req, res) => {
+  res.sendFile(path.resolve("./controller/controller.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,53 +3,21 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
-const path = require("path");
-const router = express.Router();
-// const routes = require("./routes/index");
+const routes = require("./routes/index");
+const errorHandlers = require("./handlers/errorHandlers");
 
-// For our home screen we add a route to the empty directory
-// the route simply serves the static html file in the home folder.
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/home/index.html"));
-});
 /*
-...And in order for this static HTML-file to reach its CSS- and js files (static assets), 
+...In order for the static HTML-files in our routes to reach its CSS- and js files (static assets), 
 we need to serve the home folder directory as static assets using app.use('path')
 However, this approach puts all assets in the same root folder
 Better then to add a virtual server path like this: app.use('virtual server path', 'local asset path');
 In that way, we can categorize all our assets in folders
 */
 app.use("/home", express.static(__dirname + "/home/"));
-
-// Following the same approach as our home screen, we can serve our routes and their static assets the same way
-router.get("/instructions", (req, res) => {
-  res.sendFile(path.join(__dirname + "/instructions/index.html"));
-});
 app.use("/instructions", express.static(__dirname + "/instructions/"));
 app.use("/assets", express.static(__dirname + "/assets/"));
-
-router.get("/game", (req, res) => {
-  res.sendFile(path.join(__dirname + "/game/index.html"));
-});
 app.use("/game", express.static(__dirname + "/game/"));
-
-router.get("/game", (req, res) => {
-  res.sendFile(path.join(__dirname + "/game/index.html"));
-});
-app.use("/game", express.static(__dirname + "/game/"));
-
-router.get("/controller", (req, res) => {
-  res.sendFile(path.join(__dirname + "/controller/controller.html"));
-});
-// Serve the controllers at localhost:[port]/controller/ and localhost:[port]/controllerBottom/ using static files. Spesifying the html-file since it is not called index.html.
 app.use("/controller/", express.static(__dirname + "/controller/"));
-
-/* app.get("/controllerBottom/*", function(req, res) {
-  res.sendFile(__dirname + "/controller/" + "controllerBottom.html");
-});
-app.get("/controller/*", function(req, res) {
-  res.sendFile(__dirname + "/controller/" + "controller.html");
-}); */
 
 // Handle socket stuff!
 io.on("connection", function(socket) {
@@ -66,7 +34,9 @@ io.on("connection", function(socket) {
   });
 });
 
-app.use("/", router);
+app.use("/", routes);
+
+app.use(errorHandlers.notFound);
 
 http.listen(port, "0.0.0.0", function() {
   console.log(`listening on: ${port}`);
