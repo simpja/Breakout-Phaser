e40f0162642c0c70ba9fb04f8ed4c155642bf3b5
Simen Pjaaten <simenpjaat@gmail.com>
2020-07-28 20:27:38 +0200

    made game db model

---
+++ models/Games.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const slug = require("slugs"); // Make url friendly names
+
+const gameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a game name", // instead of just a boolean, true, this will print back our specified error message
+  },
+  slug: String,
+  highscore: Number,
+});
+
+gameSchema.pre("save", function(next) {
+  if (!this.isModified("name")) {
+    next(); // skip it
+    return; // stop this function here
+  }
+  this.slug = slug(this.name); // Uses the slug package we imported at the top to generate a slug
+  next();
+  // TODO: Make more resilient so slugs are unique
+});
+
+module.exports = mongoose.model("Game", gameSchema);
